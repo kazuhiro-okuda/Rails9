d7414ee30292d35483b3b2668872b2ce753e597f
奥田一博 <okudakazuhiro@okudakazuhironoMacBook-puro.local>
2019-04-09 19:30:28 +0900

    [Finish]

---
+++ Rails9/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '747ea279cdfe2bd6203c11289c0ed4e0b4ae19a56a59d4451e88b51a7b35a17aa4d47805ec81a601d22b53a15b857a3a1f182e59bcfc49d4f2e1277b8140f273'
---
+++ Rails9/sample_app/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'posts/new'
+  get 'top' => 'root#top'
+  post 'posts' => 'posts#create'
+  get 'posts' => 'posts#index'
+  get 'posts/:id' => 'posts#show', as: 'post'
+  get 'posts/:id/edit' => 'posts#edit', as: 'edit_post'
+  patch 'post/:id' => 'posts#update', as: 'update_post'
+  delete 'post/:id' => 'posts#destroy', as: 'destroy_post'
+end
